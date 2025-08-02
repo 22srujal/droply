00bdbcb60ce2cf33dba6ed5e473c6e29d9d3851e
Srujal Sau <srujalsau@gmail.com>
2025-08-02 16:21:26 +0000

    Add sign-up and sign-in schemas, update dependencies in package.json and package-lock.json

---
+++ components/SignUpForm.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import {useForm} from "react-hook-form"
+import { useSignUp } from "@clerk/nextjs"
+import {z} from "zod"
+
+// zod custom schema
+import { signUpSchema } from "@/schemas/signUpSchema"
+
+export default function SignUpForm(){
+
+}
