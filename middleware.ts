136639ee8f49f80d3a7a6091b2d69dfbdf2cca04
Srujal Sau <srujalsau@gmail.com>
2025-08-04 14:25:45 +0000

    feat: implement authentication middleware to protect non-public routes and redirect users

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,20 @@
 import { clerkMiddleware, createRouteMatcher, auth } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"])
 
-export default clerkMiddleware()
+export default clerkMiddleware(async (auth, request) => {
+  const user = auth()
+  const userId = (await user).userId
+  const url = new URL(request.url)
+
+  if (userId && isPublicRoute(request) && url.pathname !== "/"){
+    return NextResponse.redirect(new URL("/dashboard", request.url))
+  }
+  // Protect non-public routes
+  if (!isPublicRoute(request)) {
+    await auth.protect()
+  }
+})
 
 export const config = {
   matcher: [
