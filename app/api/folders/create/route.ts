a71536af499eda521a93ad217fa35e104bb498c2
Srujal Sau <srujalsau@gmail.com>
2025-08-07 16:59:09 +0000

    fix: correct unauthorized response status codes in folder creation API

--- app/api/folders/create/route.ts
+++ app/api/folders/create/route.ts
@@ -7,17 +7,17 @@ import { NextRequest, NextResponse } from "next/server";
 import { success } from "zod/v4-mini";
 
 
-export async function Post(request: NextRequest) {
+export async function POST(request: NextRequest) {
     try {
         const {userId} =await auth()
         if(!userId){
-            return NextResponse.json({ error: "Unauthorized" }, { status: 404});
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401});
         }
         const body = await request.json()
         const {name, userId: bodyUserId, parentId = null} = body
 
         if(bodyUserId !== userId){
-            return NextResponse.json({ error: "Unauthorized" }, { status: 404});
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401});
         }
 
         if(!name || typeof name!== "string" || name.trim() === ""){
