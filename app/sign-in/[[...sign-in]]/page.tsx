7ee2d76303691440ca0ab428f6bd843bfd3aaea2
Srujal Sau <srujalsau@gmail.com>
2025-08-10 16:19:52 +0000

    feat: add authentication and file management features
    - Updated package.json to include new dependencies: @heroui/react, imagekitio-next, and next-themes.
    - Created Providers component to wrap the application with ImageKit and HeroUI providers.
    - Implemented SignInPage and SignUpPage components for user authentication.
    - Added SQL schema for files management, including fields for file metadata and user association.
    - Created snapshot and journal files for database migration tracking.

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,13 @@
+import SignInForm from '@/components/SignInForm';
+import SignUpForm from '@/components/SignUpForm'
+import React from 'react'
+
+function SignInPage() {
+    return (
+        <div>
+            <SignInForm />
+        </div>
+    );
+}
+
+export default SignInPage
